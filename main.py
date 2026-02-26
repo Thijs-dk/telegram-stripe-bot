7d47d5ec0d97521f37f79cd9659148a1bb2abea2
Thijs-dk <61691619+Thijs-dk@users.noreply.github.com>
2026-02-26 22:26:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(text, reply_markup=InlineKeyboardMarkup(keyboard))
 
-def main():
+import asyncio
+
+async def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    await app.run_polling(close_loop=False)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
